9f7d93cff98453308404496cf5296be22ef94778
sangwon514 <aq0620@naver.com>
2020-06-10 02:14:53 +0900

    프로젝트C 백엔드

---
+++ project_c/src/main/java/kr/or/connect/reservation/dto/Product.java
@@ -0,0 +1,55 @@
+package kr.or.connect.reservation.dto;
+
+
+	public class Product {
+	private Integer displayInfoId;
+	private String placeName;
+	private String productContent;
+	private String productDescription;
+	private Integer productId;
+	private String productImageUrl;
+	public Integer getDisplayInfoId() {
+		return displayInfoId;
+	}
+	public void setDisplayInfoId(Integer displayInfoId) {
+		this.displayInfoId = displayInfoId;
+	}
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public String getProductContent() {
+		return productContent;
+	}
+	public void setProductContent(String productContent) {
+		this.productContent = productContent;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductImageUrl() {
+		return productImageUrl;
+	}
+	public void setProductImageUrl(String productImageUrl) {
+		this.productImageUrl = productImageUrl;
+	}
+	@Override
+	public String toString() {
+		return "Product [displayInfoId=" + displayInfoId + ", placeName=" + placeName + ", productContent="
+				+ productContent + ", productDescription=" + productDescription + ", productId=" + productId
+				+ ", productImageUrl=" + productImageUrl + "]";
+	}
+	
+	
+}
